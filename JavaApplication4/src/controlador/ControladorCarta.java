c6019519e340e0dc3d3ae4d9625023aabf987c5b
Emanuel <emanuel.perez87@unach.mx>
2025-08-02 08:55:03 -1200

    Segundo commit

---
+++ JavaApplication4/src/controlador/ControladorCarta.java
@@ -0,0 +1,25 @@
+package controlador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author emanuel
+ */
+public class ControladorCarta {
+
+    class Maestro {
+
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        String apePaterno, apMaterno;
+        int edad;
+    }
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+
+}
